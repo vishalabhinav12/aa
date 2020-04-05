9fd02bf6d7ea35714e129d07beacede162ff476b
Vishal Kumar <63013367+vishalabhinav12@users.noreply.github.com>
2020-04-05 23:49:34 +0530

    Add files via upload

---
+++ 11.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<math.h>
+int main ()
+{
+int a, b, gcd;
+printf ("\n enter the first number :");
+scanf("%d",&a);
+printf("\n enter the second number:");
+sacnf("%d",&b);
+int I;
+for ( i=1; i<=a  && i<=b;i++)
+{
+If ((a%i==0) && (b%i==0))
+{
+gcd =I;}}
+printf ("\n gcd of number %d and %d is %d", a, b, gcd);
+printf ("\n");
+Return 0;
+}
+
